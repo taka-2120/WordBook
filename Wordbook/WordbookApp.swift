b62f8c77dfbeb1a10cc0110ae240568fd171dd03
Y.T <yu_tkhs@icloud.com>
2020-09-30 16:59:54 +0900

    Initial Commit

---
+++ Wordbook/WordbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordbookApp.swift
+//  Wordbook
+//
+//  Created by Yu Takahashi on 2020/09/30.
+//
+
+import SwiftUI
+
+@main
+struct WordbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
