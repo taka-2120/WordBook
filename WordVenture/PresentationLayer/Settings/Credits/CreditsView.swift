c00e24e9927879aaf4c1359d3033a54e29518e1f
taka-2120 <yu_tkhs@icloud.com>
2023-06-12 16:28:23 +0900

    style: localizations

--- WordVenture/PresentationLayer/Settings/Credits/CreditsView.swift
+++ WordVenture/PresentationLayer/Settings/Credits/CreditsView.swift
@@ -10,10 +10,14 @@
 import SwiftUI
 
 struct CreditsView: View {
+    private var loadingImage = try! AttributedString(markdown: "[Launch image](https://lottiefiles.com/24291-stack-of-books) is created by Scott A on LottieFiles")
+    private var authImages = try! AttributedString(markdown: "[Sign in](https://www.freepik.com/free-vector/mobile-login-concept-illustration_4957136.htm#&position=1&from_view=collections) and [sign up](https://www.freepik.com/free-vector/high-five-concept-illustration_7010860.htm#&position=0&from_view=collections) images are created by storyset on Freepik")
+    
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 15) {
-            Text("[Launch image](https://lottiefiles.com/24291-stack-of-books) is created by Scott A on LottieFiles")
-            Text("[Sign in](https://www.freepik.com/free-vector/mobile-login-concept-illustration_4957136.htm#&position=1&from_view=collections) and [sign up](https://www.freepik.com/free-vector/high-five-concept-illustration_7010860.htm#&position=0&from_view=collections) images are created by storyset on Freepik")
+            Text(loadingImage)
+            Text(authImages)
             
             Spacer()
 
