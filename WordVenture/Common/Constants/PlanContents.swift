53cc403461b6c8e44fb031f2c3890678643f7721
taka-2120 <yu_tkhs@icloud.com>
2023-06-16 09:26:25 +0900

    feat: LocalizedStringKey to String

---
+++ WordVenture/Common/Constants/PlanContents.swift
@@ -0,0 +1,8 @@
+//
+//  PlanContents.swift
+//  WordVenture
+//
+//  Created by Yu Takahashi on 6/15/23.
+//
+
+import Foundation
---
+++ WordVenture/Common/Extensions/Modifiers/View+CornerRadius.swift
@@ -0,0 +1,8 @@
+//
+//  View+CornerRadius.swift
+//  WordVenture
+//
+//  Created by Yu Takahashi on 6/13/23.
+//
+
+import Foundation
