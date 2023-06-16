b42deb02f92a09a411d9db796d554aad0f3e6521
taka-2120 <yu_tkhs@icloud.com>
2023-06-16 09:33:08 +0900

    refactor: asset symbol extensions

--- WordVenture/Common/Extensions/Color+Custom.swift
+++ WordVenture/Common/Extensions/Color+Custom.swift
@@ -10,7 +10,4 @@ import SwiftUI
 extension Color {
     static let background = Color("defaultBackground")
     static let primaryAccent = Color("AccentColor")
-    static let secondaryAccent = Color("SecondaryAccentColor")
-    static let highlight = Color("HighlightColor")
-    static let secondaryHighlight = Color("SecondaryHighlightColor")
 }
