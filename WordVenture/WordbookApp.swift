9aa3766c68edad684dda3b1c83b696eb345cc8c6
taka-2120 <yu_tkhs@icloud.com>
2023-06-19 00:58:09 +0900

    chore: remove google ads

--- WordVenture/WordbookApp.swift
+++ WordVenture/WordbookApp.swift
@@ -6,12 +6,10 @@
 //
 
 import SwiftUI
-import GoogleMobileAds
 
 @main
 struct WordbookApp: App {
     @StateObject private var screenController = ScreenController.shared
-    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     
     var body: some Scene {
         WindowGroup {
@@ -24,24 +22,5 @@ struct WordbookApp: App {
             }
             .animation(.easeInOut, value: screenController.state)
         }
-    }
-}
-
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        // Initialize Mobile Ads SDK
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        
-        #if DEBUG
-        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ GADSimulatorID ]
-        #else
-        if isTestFlight {
-            GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ GADSimulatorID ]
-        }
-        #endif
-        
-        return true
     }
 }
