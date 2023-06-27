795fca499e9727c70dafff6be749ad332aed267f
taka-2120 <yu_tkhs@icloud.com>
2023-06-27 18:13:50 +0900

    style: add DismissButton components

--- WordVenture/PresentationLayer/Settings/TermsAndConditions/TermsAndConditionsView.swift
+++ WordVenture/PresentationLayer/Settings/TermsAndConditions/TermsAndConditionsView.swift
@@ -63,20 +63,7 @@ struct TermsAndConditionsView: View {
             if selfNavigatable {
                 HStack {
                     Spacer()
-                    Button {
-                        dismiss()
-                    } label: {
-                        ZStack {
-                            Circle()
-                                .fill(Color(white: colorScheme == .dark ? 0.19 : 0.93))
-                            Image(systemName: "xmark").resizable()
-                                .scaledToFit()
-                                .font(Font.body.weight(.bold))
-                                .scaleEffect(0.416)
-                                .foregroundColor(Color(white: colorScheme == .dark ? 0.62 : 0.51))
-                        }
-                        .frame(width: 30, height: 30)
-                    }
+                    DismissButton(dismiss, colorScheme)
                 }
                 .padding()
             }
